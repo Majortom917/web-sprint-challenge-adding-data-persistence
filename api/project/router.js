069932fe160edd596bc91a71559d5bfbe3c13df8
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-01-22 05:26:38 -0800

    server, db and migrations working

--- api/project/router.js
+++ api/project/router.js
@@ -1,19 +1,19 @@
-/* // build your `/api/projects` router here
+// build your `/api/projects` router here
 const express = require('express');
 
-const Schemes = require('./scheme-model.js');
+//const Schemes = require('./scheme-model.js');
 
 const router = express.Router();
+const db = require('../data/dbConfig')
 
-router.get('/', (req, res) => {
-  Schemes.find()
-  .then(schemes => {
-    res.json(schemes);
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to get schemes' });
+router.get("/api/project", (req, res) => {
+    db('projects')
+    .then(project =>{
+        res.status(200).json(project)
+    })
+    .catch(err =>{
+        res.status(500).json(err)
+    })
   });
-});
-
-
-module.exports = router; */
+ 
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,12 +2,20 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const db = require('../data/dbConfig')
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).json({ hello: "is it working?" });
+server.get("/api/project", (req, res) => {
+  db('projects')
+  .then(project =>{
+      res.status(200).json(project)
+  })
+  .catch(err =>{
+      res.status(500).json(err)
+  })
 });
 module.exports = server;
