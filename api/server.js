2f3b172caf37f666592fa4fbd0a3f05b838f3d05
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-01-22 03:37:51 -0800

    Create a CodeGrade submission

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "is it working?" });
+});
+module.exports = server;
